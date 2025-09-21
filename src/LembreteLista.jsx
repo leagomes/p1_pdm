69d9187f15f9fc0742f1534fb368ef96b8826d94
Melyssa Moya <moyamelyssa@gmail.com>
2025-09-21 19:29:02 -0300

    feat(p1): favorita lembretes

--- src/LembreteLista.jsx
+++ src/LembreteLista.jsx
@@ -7,9 +7,14 @@ class LembreteLista extends Component {
   };
 
   adicionarLembrete = (descricao) => {
-    this.setState((estadoAnterior) => ({
-      lembretes: [...estadoAnterior.lembretes, descricao],
-    }));
+    this.setState((estadoAnterior) => {
+      const novaLista = estadoAnterior.lembretes.concat({
+        descricao: descricao,
+        favorito: false,
+      });
+
+      return { lembretes: novaLista };
+    });
   };
 
   removerLembrete = (indice) => {
@@ -26,34 +31,52 @@ class LembreteLista extends Component {
     });
   };
 
+  favoritar = (indice) => {
+    this.setState((estadoAnterior) => {
+      const novaLista = estadoAnterior.lembretes.map((item, i) => {
+        if (i === indice && item !== null) {
+          return {
+            descricao: item.descricao,
+            favorito: !item.favorito,
+          };
+        } else {
+          return item;
+        }
+      });
+
+      return { lembretes: novaLista };
+    });
+  };
+
   render() {
     return (
       <div className="container mt-5 col-3">
         <ul className="list-group mt-4">
-          {this.state.lembretes.map((lembrete, index) => {
-            if (lembrete !== null) {
-              return (
-                <li
-                  key={index}
-                  className="list-group-item d-flex justify-content-between align-items-center"
-                >
-                  <span>{lembrete}</span>
-                  <span>
-                    <i
-                      className="fa-regular fa-star me-3"
-                      style={{ color: "#FFD43B", cursor: "pointer" }}
-                    ></i>
-                    <i
-                      className="fa-solid fa-trash"
-                      style={{ color: "#0475d0", cursor: "pointer" }}
-                      onClick={() => this.removerLembrete(index)}
-                    ></i>
-                  </span>
-                </li>
-              );
-            } else {
-              return null;
-            }
+          {this.state.lembretes.map((item, i) => {
+            if (item === null) return null;
+
+            return (
+              <li
+                key={i}
+                className="list-group-item d-flex justify-content-between align-items-center"
+              >
+                <span>{item.descricao}</span>
+                <span>
+                  <i
+                    className={`me-3 ${
+                      item.favorito ? "fa-solid" : "fa-regular"
+                    } fa-star`}
+                    style={{ color: "#FFD43B", cursor: "pointer" }}
+                    onClick={() => this.favoritar(i)}
+                  ></i>
+                  <i
+                    className="fa-solid fa-trash"
+                    style={{ color: "#0475d0", cursor: "pointer" }}
+                    onClick={() => this.removerLembrete(i)}
+                  ></i>
+                </span>
+              </li>
+            );
           })}
         </ul>
         <LembreteEntrada onNovoLembrete={this.adicionarLembrete} />
